f16607884efdc2075107b94e8d092cbb26694eb6
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2022-05-29 12:43:58 +0800

    lab task 3

---
+++ 2,3.c
@@ -0,0 +1,36 @@
+/*3. Any year is input through the keyboard. Write a program to determine whether the year is a 
+leap year or not. */
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf(" Enter the year :");
+	scanf("%d",&year);
+	
+	
+	if(year %400==0)
+	{
+	printf("%d is a leap year :",year);
+		}
+	else if( year % 100==0)
+	{
+		printf("%d is not a leap year :");
+		
+	}
+	else if( year % 4==0)
+	{
+	
+	printf("%d is a leap year:");
+		
+		
+	}
+	else {
+		
+		
+		printf(" %d is not a leap yeara;");
+	}
+	
+	
+	
+	return 0;
+}
